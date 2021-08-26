0f2fa54482f1d60596e2818b4fcf34a15e8ef9e4
Zak Warsame <zakwarsamee@gmail.com>
2021-08-26 19:34:49 +0000

    implement request from npm and print out cat breed using thecatapi

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const args = process.argv.slice(2, 3);
-const url = "https://api.th3ecatapi.com/v1/breeds/search?q=";
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
 const breed = args[0];
 // https://api.thecatapi.com/v1/images/search
 
@@ -13,9 +13,5 @@ request(url + breed, function(error, response, body) {
   if (!data[0]) {
     return console.log("Breed not found");
   }
-
-  console.error("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   console.log(data[0].description);
-  console.log("body:", typeof data); // Print the HTML for the Google homepage.
 });
